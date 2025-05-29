ac5116cb70242bdd2c272e19293ee0ee2122ffed
Joel Ek <contact@joelek.se>
2025-05-29 23:28:52 +0200

    Respect retry-after header in BackendManager.

--- source/ui/managers/BackendManager.ts
+++ source/ui/managers/BackendManager.ts
@@ -4,6 +4,14 @@ import * as api from "../../api";
 export class BackendManager implements api.Client {
 	protected client: api.Client;
 	protected state: State<api.State>;
+	protected wait_until_utc: number;
+
+	protected wait(): Promise<void> {
+		let ms = Math.max(0, this.wait_until_utc - Date.now());
+		return new Promise((resolve, reject) => {
+			setTimeout(resolve, ms);
+		});
+	}
 
 	constructor(client: api.Client) {
 		this.client = client;
@@ -11,6 +19,7 @@ export class BackendManager implements api.Client {
 			type: "UNKNOWN_STATE",
 			reason: "STATE_NOT_READ"
 		});
+		this.wait_until_utc = Date.now();
 		this.readState({}).catch(() => undefined).then(async (response) => {
 			if (response != null) {
 				let payload = await response.payload();
@@ -20,11 +29,17 @@ export class BackendManager implements api.Client {
 	}
 
 	async readState(...args: Parameters<api.Client["readState"]>): ReturnType<api.Client["readState"]> {
-		return this.client.readState(...args);
+		await this.wait();
+		let response = await this.client.readState(...args);
+		this.wait_until_utc = Date.now() + response.headers()["retry-after"] * 1000;
+		return response;
 	}
 
 	async sendCommand(...args: Parameters<api.Client["sendCommand"]>): ReturnType<api.Client["sendCommand"]> {
-		return this.client.sendCommand(...args);
+		await this.wait();
+		let response = await this.client.sendCommand(...args);
+		this.wait_until_utc = Date.now() + response.headers()["retry-after"] * 1000;
+		return response;
 	}
 
 	getState(): State<api.State> {
