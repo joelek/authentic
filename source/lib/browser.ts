cfdd4e8e072bef0adce1813b16991104d8888d7e
Joel Ek <contact@joelek.se>
2025-06-05 09:12:24 +0200

    Export all subprojects in lib.

---
+++ source/lib/browser.ts
@@ -0,0 +1,4 @@
+export * as api from "../api/client";
+export * as client from "../client";
+export * as shared from "../shared";
+export * as ui from "../ui";
---
+++ source/lib/node.ts
@@ -0,0 +1,4 @@
+export * as api from "../api/server";
+export * as email from "../email";
+export * as shared from "../shared";
+export * as server from "../server";
