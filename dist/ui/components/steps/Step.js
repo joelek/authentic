287ecf20e607e49881070a760e39cb4c14830258
Joel Ek <contact@joelek.se>
2025-06-09 08:02:44 +0200

    Generate dist artifacts.

--- dist/ui/components/steps/Step.js
+++ dist/ui/components/steps/Step.js
@@ -5,16 +5,24 @@ const bonsai_1 = require("@joelek/bonsai");
 const Block_1 = require("../Block");
 document.head.appendChild(bonsai_1.html.style({}, `\
 	.step {
-		display: grid;
-		gap: 24px;
+		transition: all 0.50s;
 	}
 
 	.step--visible {
-
+		height: auto;
+		opacity: 1.0;
+		transform: translate(0px, 0px);
 	}
 
 	.step--hidden {
-		display: none;
+		height: 0px;
+		opacity: 0.0;
+		transform: translate(0px, 120px);
+	}
+
+	.step__content {
+		display: grid;
+		gap: 24px;
 	}
 `));
 function Step(managers, attributes, ...children) {
@@ -23,7 +31,9 @@ function Step(managers, attributes, ...children) {
     let visible = type.compute((type) => type != null);
     return ((0, Block_1.Block)("div", {
         class: ["step", visible.compute((visible) => visible ? "step--visible" : "step--hidden")]
-    }, ...children));
+    }, (0, Block_1.Block)("div", {
+        class: ["step__content"]
+    }, ...children)));
 }
 exports.Step = Step;
 ;
