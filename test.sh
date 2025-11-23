832b902dc1ee6473a6e5a24c25b79fc9b5c92e8b
Joel Ek <contact@joelek.se>
2025-11-23 10:49:42 +0100

    Make email configurable in test script.

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@
 curl -i http://localhost:8080/auth/
 sleep 1
 read SESSION
-curl -i http://localhost:8080/auth/ -H "Cookie: session=$SESSION" --data '{"command":{"type":"REGISTER"}}'
+curl -i http://localhost:8080/auth/ -H "Cookie: session=$SESSION" --data "{\"command\":{\"type\":\"REGISTER\"}}"
 sleep 1
-curl -i http://localhost:8080/auth/ -H "Cookie: session=$SESSION" --data '{"command":{"type":"REGISTER_EMAIL","email":"joel@rydaform.se"}}'
+read EMAIL
+curl -i http://localhost:8080/auth/ -H "Cookie: session=$SESSION" --data "{\"command\":{\"type\":\"REGISTER_EMAIL\",\"email\":\"$EMAIL\"}}"
 sleep 1
 read CODE
 curl -i http://localhost:8080/auth/ -H "Cookie: session=$SESSION" --data "{\"command\":{\"type\":\"REGISTER_CODE\",\"code\":\"$CODE\"}}"
