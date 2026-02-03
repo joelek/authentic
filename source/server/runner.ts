40951b931d7a6c103fe63135030005eb688e7a1e
Joel Ek <contact@joelek.se>
2026-02-03 21:32:11 +0100

    Split start() into startBroker() and startWorker().

--- source/server/runner.ts
+++ source/server/runner.ts
@@ -88,6 +88,170 @@ export class Runner {
 		console.log(`Job with type ${job.type} and id ${job.job_id} completed.`);
 	}
 
+	protected async startBroker(): Promise<void> {
+		let promises: Array<Promise<void>> = [];
+		for (let type in this.tasks) {
+			let scheduler = new Promise<void>(async (resolve, reject) => {
+				let scheduler = this.tasks[type].scheduler;
+				if (scheduler != null) {
+					for await (let scheduled_job of this.generateScheduledJobs(scheduler)) {
+						let enqueued_jobs = await this.jobs.lookupObjects({
+							where: {
+								all: [
+									{
+										key: "status",
+										operator: "==",
+										operand: "ENQUEUED"
+									},
+									{
+										key: "type",
+										operator: "==",
+										operand: type
+									}
+								]
+							},
+							length: 1
+						});
+						if (enqueued_jobs.length > 0) {
+							continue;
+						}
+						let running_jobs = await this.jobs.lookupObjects({
+							where: {
+								all: [
+									{
+										key: "status",
+										operator: "==",
+										operand: "RUNNING"
+									},
+									{
+										key: "type",
+										operator: "==",
+										operand: type
+									}
+								]
+							},
+							length: 1
+						});
+						if (running_jobs.length > 0) {
+							continue;
+						}
+						await this.enqueue(type, scheduled_job.metadata);
+					}
+				}
+				resolve();
+			});
+			promises.push(scheduler);
+		}
+		let poller = new Promise<void>(async (resolve, reject) => {
+			for await (let date of this.generateDates(oneSecondFromNow)) {
+				let jobs = await this.jobs.lookupObjects({
+					where: {
+						all: [
+							{
+								key: "status",
+								operator: "==",
+								operand: "ENQUEUED"
+							},
+							{
+								any: [
+									{
+										key: "expires_utc",
+										operator: "==",
+										operand: null
+									},
+									{
+										key: "expires_utc",
+										operator: ">",
+										operand: Date.now()
+									}
+								]
+							}
+						]
+					},
+					order: {
+						keys: ["created_utc"],
+						sort: "ASC"
+					},
+					length: 1
+				});
+				let job = jobs.pop();
+				if (job == null) {
+					continue;
+				}
+				await this.runJob(job);
+			}
+			resolve();
+		});
+		promises.push(poller);
+		let cleaner = new Promise<void>(async (resolve, reject) => {
+			for await (let date of this.generateDates(oneMinuteFromNow)) {
+				let jobs = await this.jobs.lookupObjects({
+					where: {
+						all: [
+							{
+								key: "status",
+								operator: "!=",
+								operand: "RUNNING"
+							},
+							{
+								key: "expires_utc",
+								operator: "<=",
+								operand: Date.now()
+							}
+						]
+					}
+				});
+				for (let job of jobs) {
+					await this.jobs.deleteObject(job.job_id).catch(() => undefined);
+				}
+			}
+			resolve();
+		});
+		promises.push(cleaner);
+		await Promise.all(promises);
+	}
+
+	protected async startWorker(): Promise<void> {
+		if (process.argv.length === 3) {
+			let job_id = process.argv[2];
+			let job = await this.jobs.lookupObject(job_id);
+			if (job.status === "ENQUEUED") {
+				if (job.type in this.tasks) {
+					job = await this.jobs.updateObject({
+						...job,
+						status: "RUNNING",
+						updated_utc: Date.now(),
+						started_utc: Date.now()
+					});
+					try {
+						await this.tasks[job.type].runner(job.job_id, job.options ?? null);
+						job = await this.jobs.updateObject({
+							...job,
+							status: "SUCCESS",
+							updated_utc: Date.now(),
+							ended_utc: Date.now()
+						});
+					} catch (error) {
+						job = await this.jobs.updateObject({
+							...job,
+							status: "FAILURE",
+							updated_utc: Date.now(),
+							ended_utc: Date.now()
+						});
+					}
+				} else {
+					job = await this.jobs.updateObject({
+						...job,
+						status: "INVALID",
+						updated_utc: Date.now()
+					});
+				}
+			}
+		}
+		// Exit explicitly to close open database connections.
+		process.exit();
+	}
+
 	constructor(options?: RunnerOptions) {
 		this.tasks = options?.tasks ?? {};
 		this.jobs = options?.jobs ?? new VolatileJobStore();
@@ -113,167 +277,11 @@ export class Runner {
 		return job.job_id;
 	}
 
-	async start(): Promise<void> {
+	start(): Promise<void> {
 		if (this.isMainThread()) {
-			let promises: Array<Promise<void>> = [];
-			for (let type in this.tasks) {
-				let scheduler = new Promise<void>(async (resolve, reject) => {
-					let scheduler = this.tasks[type].scheduler;
-					if (scheduler != null) {
-						for await (let scheduled_job of this.generateScheduledJobs(scheduler)) {
-							let enqueued_jobs = await this.jobs.lookupObjects({
-								where: {
-									all: [
-										{
-											key: "status",
-											operator: "==",
-											operand: "ENQUEUED"
-										},
-										{
-											key: "type",
-											operator: "==",
-											operand: type
-										}
-									]
-								},
-								length: 1
-							});
-							if (enqueued_jobs.length > 0) {
-								continue;
-							}
-							let running_jobs = await this.jobs.lookupObjects({
-								where: {
-									all: [
-										{
-											key: "status",
-											operator: "==",
-											operand: "RUNNING"
-										},
-										{
-											key: "type",
-											operator: "==",
-											operand: type
-										}
-									]
-								},
-								length: 1
-							});
-							if (running_jobs.length > 0) {
-								continue;
-							}
-							await this.enqueue(type, scheduled_job.metadata);
-						}
-					}
-					resolve();
-				});
-				promises.push(scheduler);
-			}
-			let poller = new Promise<void>(async (resolve, reject) => {
-				for await (let date of this.generateDates(oneSecondFromNow)) {
-					let jobs = await this.jobs.lookupObjects({
-						where: {
-							all: [
-								{
-									key: "status",
-									operator: "==",
-									operand: "ENQUEUED"
-								},
-								{
-									any: [
-										{
-											key: "expires_utc",
-											operator: "==",
-											operand: null
-										},
-										{
-											key: "expires_utc",
-											operator: ">",
-											operand: Date.now()
-										}
-									]
-								}
-							]
-						},
-						order: {
-							keys: ["created_utc"],
-							sort: "ASC"
-						},
-						length: 1
-					});
-					let job = jobs.pop();
-					if (job == null) {
-						continue;
-					}
-					await this.runJob(job);
-				}
-				resolve();
-			});
-			promises.push(poller);
-			let cleaner = new Promise<void>(async (resolve, reject) => {
-				for await (let date of this.generateDates(oneMinuteFromNow)) {
-					let jobs = await this.jobs.lookupObjects({
-						where: {
-							all: [
-								{
-									key: "status",
-									operator: "!=",
-									operand: "RUNNING"
-								},
-								{
-									key: "expires_utc",
-									operator: "<=",
-									operand: Date.now()
-								}
-							]
-						}
-					});
-					for (let job of jobs) {
-						await this.jobs.deleteObject(job.job_id).catch(() => undefined);
-					}
-				}
-				resolve();
-			});
-			promises.push(cleaner);
-			await Promise.all(promises);
+			return this.startBroker();
 		} else {
-			if (process.argv.length === 3) {
-				let job_id = process.argv[2];
-				let job = await this.jobs.lookupObject(job_id);
-				if (job.status === "ENQUEUED") {
-					if (job.type in this.tasks) {
-						job = await this.jobs.updateObject({
-							...job,
-							status: "RUNNING",
-							updated_utc: Date.now(),
-							started_utc: Date.now()
-						});
-						try {
-							await this.tasks[job.type].runner(job.job_id, job.options ?? null);
-							job = await this.jobs.updateObject({
-								...job,
-								status: "SUCCESS",
-								updated_utc: Date.now(),
-								ended_utc: Date.now()
-							});
-						} catch (error) {
-							job = await this.jobs.updateObject({
-								...job,
-								status: "FAILURE",
-								updated_utc: Date.now(),
-								ended_utc: Date.now()
-							});
-						}
-					} else {
-						job = await this.jobs.updateObject({
-							...job,
-							status: "INVALID",
-							updated_utc: Date.now()
-						});
-					}
-				}
-			}
-			// Exit explicitly to close open database connections.
-			process.exit();
+			return this.startWorker();
 		}
 	}
 };
