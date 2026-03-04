824d2335c314988064d06151850feb76a90458a9
Joel Ek <contact@joelek.se>
2026-03-04 23:08:56 +0100

    Add test subproject.

---
+++ source/test/mariadb.test.ts
@@ -0,0 +1,47 @@
+import * as wtf from "@joelek/wtf";
+import * as server from "../server";
+import * as mariadb from "./mariadb";
+import * as objects from "./objects";
+
+for (let operator of [">", ">=", "==", "!=", "<", "<="] as const) {
+	for (let sort of ["ASC", "DESC"] as const) {
+		wtf.test(`Stores should support integer lookup filters for operator ${operator} and sort ${sort}.`, async (assert) => {
+			let store = new server.stores.store.VolatileObjectStore("object_id", [], objects.Object, {});
+			for (let object of await mariadb.OBJECTS.lookupObjects()) {
+				store.createObject(object);
+			}
+			let expected = await store.lookupObjects({
+				where: {
+					key: "optional_integer",
+					operator: operator,
+					operand: 5
+				},
+				order: {
+					keys: ["optional_integer", "object_id"],
+					sort: sort
+				}
+			});
+			let observed: Array<objects.Object> = [];
+			while (true) {
+				let objects = await mariadb.OBJECTS.lookupObjects({
+					where: {
+						key: "optional_integer",
+						operator: operator,
+						operand: 5
+					},
+					order: {
+						keys: ["optional_integer", "object_id"],
+						sort: sort
+					},
+					anchor: observed[observed.length - 1]?.object_id,
+					length: 1
+				});
+				if (objects.length === 0) {
+					break;
+				}
+				observed.push(...objects);
+			}
+			assert.equals(observed, expected);
+		});
+	}
+}
