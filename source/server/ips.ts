526328279464b852d215996ab578c3121fbe94df
Joel Ek <contact@joelek.se>
2026-05-30 21:59:41 +0200

    Properly normalize IPv6 mapped IPv4 addresses.

--- source/server/ips.ts
+++ source/server/ips.ts
@@ -4,6 +4,17 @@ export function normalizeIPv6(ip: string): string {
 	if (!libnet.isIPv6(ip)) {
 		throw new Error(`Expected "${ip}" to be a valid IPv6 address!`);
 	}
+	let lastColonPosition = ip.lastIndexOf(":");
+	if (lastColonPosition >= 0) {
+		let prefix = ip.slice(0, lastColonPosition);
+		let suffix = ip.slice(lastColonPosition + 1);
+		if (libnet.isIPv4(suffix)) {
+			let hex = suffix.split(".").map((part) => Number.parseInt(part, 10).toString(16).padStart(2, "0")).join("");
+			let one = hex.slice(0, 4);
+			let two = hex.slice(4, 8);
+			ip = `${prefix}:${one}:${two}`;
+		}
+	}
 	let groups = new Array<string>();
 	let position = ip.indexOf("::");
 	if (position >= 0) {
